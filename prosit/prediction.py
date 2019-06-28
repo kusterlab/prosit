38ed137101aaa5f65062834c6fe9343f09ddcb0b
Siegfried Gessulat <s.gessulat@gmail.com>
2019-06-28 21:01:20 +0200

    :lipstick: :sparkles: integrate iRT model

--- prosit/prediction.py
+++ prosit/prediction.py
@@ -1,4 +1,6 @@
 import os
+import keras
+import numpy as np
 
 from . import model as model_lib
 from . import io
@@ -6,40 +8,23 @@ from . import constants
 from . import sanitize
 
 
-def predict(tensor, model, model_config, verbose=False):
-    import keras
+def predict(data, d_model):
     # check for mandatory keys
-    x = io.get_array(tensor, model_config["x"])
+    x = io.get_array(data, d_model["config"]["x"])
 
-    model.compile(optimizer="adam", loss="mse")
-    prediction = model.predict(
-        x, verbose=verbose, batch_size=constants.PRED_BATCH_SIZE
-    )
-    if model_config["prediction_type"] == "intensity":
-        tensor["intensities_pred"] = prediction
-        tensor = sanitize.prediction(tensor)
-    elif model_config["prediction_type"] == "iRT":
-        import numpy as np
-        tensor["iRT"] = prediction * np.sqrt(float(model_config["iRT_rescaling_var"])) + float(model_config["iRT_rescaling_mean"])
+    keras.backend.set_session(d_model["session"])
+    with d_model["graph"].as_default():
+        prediction = d_model["model"].predict(
+            x, verbose=True, batch_size=constants.PRED_BATCH_SIZE
+        )
+
+    if d_model["config"]["prediction_type"] == "intensity":
+        data["intensities_pred"] = prediction
+        data = sanitize.prediction(data)
+    elif d_model["config"]["prediction_type"] == "iRT":
+        scal = float(d_model["config"]["iRT_rescaling_var"])
+        mean = float(d_model["config"]["iRT_rescaling_mean"])
+        data["iRT"] = prediction * np.sqrt(scal) + mean
     else:
         raise ValueError("model_config misses parameter")
-
-    return tensor
-
-
-if __name__ == "__main__":
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # turn off tf logging
-    data_path = constants.DATA_PATH
-    model_dir = constants.MODEL_DIR
-
-    weights_path = model_lib.get_best_weights_path(model_dir)
-    weights_name = weights_path.split("/")[-1][:-5]
-    data_name = data_path.split("/")[-1][:-5]
-    model, model_config = model_lib.load(model_dir, trained=True)
-    tensor = io.from_hdf5(data_path)
-    # tensor here is a dictionary
-    tensor = predict(tensor, model, model_config, verbose=True)
-
-    path = os.path.join(constants.OUT_DIR, "prediction.hdf5")
-    # tensor here is a dictionary
-    io.to_hdf5(tensor, path)
+    return data
