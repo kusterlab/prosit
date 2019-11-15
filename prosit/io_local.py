f69cc63b8319952bb75a0f02889a7d3fa439b52d
Michelle Gill <michelle@michellelynngill.com>
2019-11-15 12:58:22 -0500

    Use memory mapped arrays for data loading

--- prosit/io_local.py
+++ prosit/io_local.py
@@ -14,9 +14,16 @@ def to_hdf5(dictionary, path):
             f.create_dataset(key, data=data, dtype=data.dtype, compression="gzip")
 
 
-def from_hdf5(path):
+def from_hdf5(path, n_samples=None):
+    from keras.utils import HDF5Matrix
     import h5py
-
-    with h5py.File(path, "r") as f:
-        data = {k: f[k][...] for k in f.keys()}
+    
+    # Get a list of the keys for the datasets
+    with h5py.File(path, 'r') as f:
+        dataset_list = list(f.keys())
+    
+    # Assemble into a dictionary
+    data = dict()
+    for dataset in dataset_list:
+        data[dataset] = HDF5Matrix(path, dataset, start=0, end=n_samples, normalizer=None)
     return data
